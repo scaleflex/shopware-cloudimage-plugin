fbe96485e05db6572f0fcc74822da8d8db3bcb8b
Duc Anh Nguyen <ducanh.2206@gmail.com>
2022-09-27 10:39:22 +0700

    update

--- src/Component/Profiler/TwigDecorator.php
+++ src/Component/Profiler/TwigDecorator.php
@@ -2,9 +2,9 @@
 
 namespace CloudImage\Component\Profiler;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Twig\Environment;
 use Twig\TemplateWrapper;
-use CloudImage\Helper\Helper;
 
 class TwigDecorator extends Environment
 {
@@ -22,13 +22,51 @@ class TwigDecorator extends Environment
             $this->renders[$name] = $context;
         }
         $pageContent = parent::render($template, $context);
-        $helper = new Helper();
-        $pageContent = $helper->overwriteImgTag($pageContent);
-        return parent::render($template, $context);
+
+        return $this->overwriteImgTag($pageContent);
     }
 
     public function getTemplateData(): array
     {
         return $this->renders;
+    }
+
+    private function overwriteImgTag(string $pageContent): string
+    {
+        if (stripos($pageContent, '<img') !== false) {
+            $dom = new \DOMDocument();
+            $useErrors = libxml_use_internal_errors(true);
+            $dom->loadHTML('<?xml encoding="utf-8" ?>' . $pageContent);
+            libxml_use_internal_errors($useErrors);
+            $dom->preserveWhiteSpace = false;
+            $replaceHtml = false;
+//            $quality = '';
+//            if ($config->get('CloudImage.config.ciImageQuality') < 100) {
+//                $quality = '?q=' . $config->get('CloudImage.config.ciImageQuality');
+//            }
+            $quality = '?q=90';
+            //$ignoreSvg = $config->get('CloudImage.config.ciIgnoreSvgImage');
+            $ignoreSvg = true;
+
+            foreach ($dom->getElementsByTagName('img') as $element) {
+                /** @var DOMElement $element */
+                if ($element->hasAttribute('src')) {
+                    if ($ignoreSvg && strtolower(pathinfo($element->getAttribute('src'), PATHINFO_EXTENSION)) === 'svg') {
+                        continue;
+                    }
+
+                    $element->setAttribute('ci-src', $element->getAttribute('src') . $quality);
+                    $element->removeAttribute('src');
+                    $replaceHtml = true;
+                }
+            }
+
+            if ($replaceHtml) {
+                $pageContent = $dom->saveHTML($dom->documentElement);
+                $pageContent = str_ireplace(['<html><body>', '</body></html>'], '', $pageContent);
+            }
+        }
+
+        return $pageContent;
     }
 }
