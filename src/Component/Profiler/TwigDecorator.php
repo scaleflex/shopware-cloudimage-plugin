61dc41cc5e254aa8f7639a872324b9ba3f4b6968
bavuong0810 <bavuong0810@gmail.com>
2022-09-28 18:42:13 +0700

    complete config in twig decorator

--- src/Component/Profiler/TwigDecorator.php
+++ src/Component/Profiler/TwigDecorator.php
@@ -2,9 +2,6 @@
 
 namespace CloudImage\Component\Profiler;
 
-use Doctrine\DBAL\Connection;
-use Doctrine\DBAL\Driver\AbstractSQLServerDriver\Exception\PortWithoutHost;
-use Shopware\Core\Maintenance\System\Struct\DatabaseConnectionInformation;
 use Twig\Environment;
 use Twig\TemplateWrapper;
 
@@ -40,43 +37,49 @@ class TwigDecorator extends Environment
                     FROM system_config 
                     WHERE configuration_key LIKE 'CloudImage.config%'");
         $config = $query->fetchAllAssociative();
-        dd($config);
+        $arrConfig = [];
+        foreach ($config as $item) {
+            $itemValue = json_decode($item['configuration_value'], true);
+            $arrConfig[$item['configuration_key']] = $itemValue['_value'];
+        }
 
-        if (stripos($pageContent, '<img') !== false) {
-            $dom = new \DOMDocument();
-            $useErrors = libxml_use_internal_errors(true);
-            $dom->loadHTML('<?xml encoding="utf-8" ?>' . $pageContent);
-            libxml_use_internal_errors($useErrors);
-            $dom->preserveWhiteSpace = false;
-            $replaceHtml = false;
-//            $quality = '';
-//            if ($config->get('CloudImage.config.ciImageQuality') < 100) {
-//                $quality = '?q=' . $config->get('CloudImage.config.ciImageQuality');
-//            }
-            $quality = '?q=90';
-            //$ignoreSvg = $config->get('CloudImage.config.ciIgnoreSvgImage');
-            $ignoreSvg = true;
+        if ($arrConfig['CloudImage.config.ciActivation'] && isset($arrConfig['CloudImage.config.ciToken'])
+            && $arrConfig['CloudImage.config.ciToken'] != '') {
+            if (stripos($pageContent, '<img') !== false) {
+                $dom = new \DOMDocument();
+                $useErrors = libxml_use_internal_errors(true);
+                $dom->loadHTML('<?xml encoding="utf-8" ?>' . $pageContent);
+                libxml_use_internal_errors($useErrors);
+                $dom->preserveWhiteSpace = false;
+                $replaceHtml = false;
+                $quality = '';
+                if (isset($arrConfig['CloudImage.config.ciImageQuality']) && $arrConfig['CloudImage.config.ciImageQuality'] < 100) {
+                    $quality = '?q=' . $arrConfig['CloudImage.config.ciImageQuality'];
+                }
 
-            foreach ($dom->getElementsByTagName('img') as $element) {
-                /** @var DOMElement $element */
-                if ($element->hasAttribute('src')) {
-                    if ($ignoreSvg && strtolower(pathinfo($element->getAttribute('src'), PATHINFO_EXTENSION)) === 'svg') {
-                        continue;
+                $ignoreSvg = (isset($arrConfig['CloudImage.config.ciIgnoreSvgImage'])) ? $arrConfig['CloudImage.config.ciIgnoreSvgImage'] : false;
+
+                foreach ($dom->getElementsByTagName('img') as $element) {
+                    /** @var DOMElement $element */
+                    if ($element->hasAttribute('src')) {
+                        if ($ignoreSvg && strtolower(pathinfo($element->getAttribute('src'), PATHINFO_EXTENSION)) === 'svg') {
+                            continue;
+                        }
+
+                        $element->setAttribute('ci-src', $element->getAttribute('src') . $quality);
+                        $element->removeAttribute('src');
+                        $replaceHtml = true;
                     }
 
-                    $element->setAttribute('ci-src', $element->getAttribute('src') . $quality);
-                    $element->removeAttribute('src');
-                    $replaceHtml = true;
+                    if ($element->hasAttribute('srcset')) {
+                        $element->removeAttribute('srcset');
+                    }
                 }
 
-                if ($element->hasAttribute('srcset')) {
-                    $element->removeAttribute('srcset');
+                if ($replaceHtml) {
+                    $pageContent = $dom->saveHTML($dom->documentElement);
+                    $pageContent = str_ireplace(['<html><body>', '</body></html>'], '', $pageContent);
                 }
-            }
-
-            if ($replaceHtml) {
-                $pageContent = $dom->saveHTML($dom->documentElement);
-                $pageContent = str_ireplace(['<html><body>', '</body></html>'], '', $pageContent);
             }
         }
 
