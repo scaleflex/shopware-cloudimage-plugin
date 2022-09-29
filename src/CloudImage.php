19f375b26ccb27609c63766e164ba94f444389d9
bavuong0810 <62991507+bavuong0810@users.noreply.github.com>
2022-09-29 18:33:54 +0700

    update default config & reload CloudImage JS when open cart modal

--- src/CloudImage.php
+++ src/CloudImage.php
@@ -24,8 +24,8 @@ class CloudImage extends Plugin
         $config = $this->container->get('Shopware\Core\System\SystemConfig\SystemConfigService');
 
         //set the specified values as defaults
-        $config->set('CloudImage.config.ciMaximumPixelRatio', 2);
-        $config->set('CloudImage.config.ciImageQuality', 90);
+        $config->set('CloudImage.config.ciMaximumPixelRatio', "2");
+        $config->set('CloudImage.config.ciImageQuality', "90");
     }
 
     public function activate(ActivateContext $activateContext): void
@@ -35,7 +35,7 @@ class CloudImage extends Plugin
         $config = $this->container->get('Shopware\Core\System\SystemConfig\SystemConfigService');
 
         //set the specified values as defaults
-        $config->set('CloudImage.config.ciMaximumPixelRatio', 2);
-        $config->set('CloudImage.config.ciImageQuality', 90);
+        $config->set('CloudImage.config.ciMaximumPixelRatio', "2");
+        $config->set('CloudImage.config.ciImageQuality', "90");
     }
 }
