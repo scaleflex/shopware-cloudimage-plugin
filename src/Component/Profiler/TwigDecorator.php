2f9618f42f2c961ae70b8974ec6ee9d4877ffaae
Vương Đỗ <bavuong0810@gmail.com>
2022-11-05 12:51:28 +0700

    fix issue changing saleschannel

--- src/Component/Profiler/TwigDecorator.php
+++ src/Component/Profiler/TwigDecorator.php
@@ -4,6 +4,7 @@ namespace Scaleflex\Cloudimage\Component\Profiler;
 
 use Twig\Environment;
 use Twig\TemplateWrapper;
+use Shopware\Core\Framework\Uuid\Uuid;
 
 class TwigDecorator extends Environment
 {
@@ -22,7 +23,10 @@ class TwigDecorator extends Environment
         }
         $pageContent = parent::render($template, $context);
 
-        return $this->overwriteImgTag($pageContent);
+        $jsonContext = json_encode($context['context']);
+        $arrContext = json_decode($jsonContext, true);
+        $salesChannelId = (string)$arrContext['salesChannel']['id'];
+        return $this->overwriteImgTag($pageContent, $salesChannelId);
     }
 
     public function getTemplateData(): array
@@ -30,55 +34,60 @@ class TwigDecorator extends Environment
         return $this->renders;
     }
 
-    private function overwriteImgTag(string $pageContent): string
+    private function overwriteImgTag(string $pageContent, string $salesChannelId): string
     {
-        $connection = \Shopware\Core\Kernel::getConnection();
-        $query = $connection->executeQuery("SELECT configuration_key, configuration_value 
+        if ($salesChannelId != '') {
+            $connection = \Shopware\Core\Kernel::getConnection();
+            $query = $connection->executeQuery("SELECT configuration_key, configuration_value 
                     FROM system_config 
-                    WHERE configuration_key LIKE 'ScaleflexCloudimage.config%'");
-        $config = $query->fetchAllAssociative();
-        $arrConfig = [];
-        foreach ($config as $item) {
-            $itemValue = json_decode($item['configuration_value'], true);
-            $arrConfig[$item['configuration_key']] = $itemValue['_value'];
-        }
+                    WHERE configuration_key LIKE 'ScaleflexCloudimage.config%' AND sales_channel_id = '" . Uuid::fromHexToBytes($salesChannelId) . "'");
+            $config = $query->fetchAllAssociative();
 
-        if ($arrConfig['ScaleflexCloudimage.config.ciActivation'] && isset($arrConfig['ScaleflexCloudimage.config.ciToken'])
-            && $arrConfig['ScaleflexCloudimage.config.ciToken'] != '') {
-            if (stripos($pageContent, '<img') !== false) {
-                $dom = new \DOMDocument();
-                $useErrors = libxml_use_internal_errors(true);
-                $dom->loadHTML('<?xml encoding="utf-8" ?>' . $pageContent);
-                libxml_use_internal_errors($useErrors);
-                $dom->preserveWhiteSpace = false;
-                $replaceHtml = false;
-                $quality = '';
-                if (isset($arrConfig['ScaleflexCloudimage.config.ciImageQuality']) && $arrConfig['ScaleflexCloudimage.config.ciImageQuality'] < 100) {
-                    $quality = '?q=' . $arrConfig['ScaleflexCloudimage.config.ciImageQuality'];
+            if (count($config) > 0) {
+                $arrConfig = [];
+                foreach ($config as $item) {
+                    $itemValue = json_decode($item['configuration_value'], true);
+                    $arrConfig[$item['configuration_key']] = $itemValue['_value'];
                 }
 
-                $ignoreSvg = (isset($arrConfig['ScaleflexCloudimage.config.ciIgnoreSvgImage'])) ? $arrConfig['ScaleflexCloudimage.config.ciIgnoreSvgImage'] : false;
-
-                foreach ($dom->getElementsByTagName('img') as $element) {
-                    /** @var DOMElement $element */
-                    if ($element->hasAttribute('src')) {
-                        if ($ignoreSvg && strtolower(pathinfo($element->getAttribute('src'), PATHINFO_EXTENSION)) === 'svg') {
-                            continue;
+                if ($arrConfig['ScaleflexCloudimage.config.ciActivation'] && isset($arrConfig['ScaleflexCloudimage.config.ciToken'])
+                    && $arrConfig['ScaleflexCloudimage.config.ciToken'] != '') {
+                    if (stripos($pageContent, '<img') !== false) {
+                        $dom = new \DOMDocument();
+                        $useErrors = libxml_use_internal_errors(true);
+                        $dom->loadHTML('<?xml encoding="utf-8" ?>' . $pageContent);
+                        libxml_use_internal_errors($useErrors);
+                        $dom->preserveWhiteSpace = false;
+                        $replaceHtml = false;
+                        $quality = '';
+                        if (isset($arrConfig['ScaleflexCloudimage.config.ciImageQuality']) && $arrConfig['ScaleflexCloudimage.config.ciImageQuality'] < 100) {
+                            $quality = '?q=' . $arrConfig['ScaleflexCloudimage.config.ciImageQuality'];
                         }
 
-                        $element->setAttribute('ci-src', $element->getAttribute('src') . $quality);
-                        $element->removeAttribute('src');
-                        $replaceHtml = true;
-                    }
+                        $ignoreSvg = (isset($arrConfig['ScaleflexCloudimage.config.ciIgnoreSvgImage'])) ? $arrConfig['ScaleflexCloudimage.config.ciIgnoreSvgImage'] : false;
 
-                    if ($element->hasAttribute('srcset')) {
-                        $element->removeAttribute('srcset');
-                    }
-                }
+                        foreach ($dom->getElementsByTagName('img') as $element) {
+                            /** @var DOMElement $element */
+                            if ($element->hasAttribute('src')) {
+                                if ($ignoreSvg && strtolower(pathinfo($element->getAttribute('src'), PATHINFO_EXTENSION)) === 'svg') {
+                                    continue;
+                                }
 
-                if ($replaceHtml) {
-                    $pageContent = $dom->saveHTML($dom->documentElement);
-                    $pageContent = str_ireplace(['<html><body>', '</body></html>'], '', $pageContent);
+                                $element->setAttribute('ci-src', $element->getAttribute('src') . $quality);
+                                $element->removeAttribute('src');
+                                $replaceHtml = true;
+                            }
+
+                            if ($element->hasAttribute('srcset')) {
+                                $element->removeAttribute('srcset');
+                            }
+                        }
+
+                        if ($replaceHtml) {
+                            $pageContent = $dom->saveHTML($dom->documentElement);
+                            $pageContent = str_ireplace(['<html><body>', '</body></html>'], '', $pageContent);
+                        }
+                    }
                 }
             }
         }
